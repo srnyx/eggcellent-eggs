35cb27cb221c322f5a17c3eff809f34193c3fe33
srnyx <25808801+srnyx@users.noreply.github.com>
2025-04-15 19:05:43 -0400

    Fix #1
    It will now detect credential changes on startup and make the necessary changes to use the new credentials

--- Eggs/Git-Python Application/startup.sh
+++ Eggs/Git-Python Application/startup.sh
@@ -1,5 +1,25 @@
 # Pull from git if enabled
 if [[ -d .git && "{{PULL_START}}" == "1" ]]; then
+  # Extract authentication from origin URL
+  ORIGIN_URL=$(git config --get remote.origin.url);
+  ORIGIN_CREDENTIALS=$(echo "$ORIGIN_URL" | grep -oP '(?<=//)[^@]+' || echo "");
+  ORIGIN_USERNAME=$(echo "$ORIGIN_CREDENTIALS" | cut -d':' -f1 || echo "");
+  ORIGIN_PASSWORD=$(echo "$ORIGIN_CREDENTIALS" | cut -d':' -f2 || echo "");
+
+  # If origin URL doesn't have username/password but new ones are provided, add them
+  if [[ ( "$ORIGIN_USERNAME" == "$ORIGIN_CREDENTIALS" || "$ORIGIN_PASSWORD" == "$ORIGIN_CREDENTIALS" ) && ( -n "{{GIT_USERNAME}}" || -n "{{GIT_TOKEN}}" ) ]]; then
+    echo "Detected credentials! Inserting them into origin URL...";
+    git remote set-url origin "$(echo "$ORIGIN_URL" | sed -E "s|//|//{{GIT_USERNAME}}:{{GIT_TOKEN}}@|")";
+  else
+    # If usernames or passwords are different, update the origin URL
+    if [[ "$ORIGIN_USERNAME" != "{{GIT_USERNAME}}" || "$ORIGIN_PASSWORD" != "{{GIT_TOKEN}}" ]]; then
+      echo "Detected new credentials! Inserting them into origin URL...";
+      git remote set-url origin "$(echo "$ORIGIN_URL" | sed -E "s|//[^@]+@|//{{GIT_USERNAME}}:{{GIT_TOKEN}}@|")";
+    fi;
+  fi;
+
+  # Pull
+  echo "Pulling latest from git...";
   GIT_OUTPUT=$(git pull);
   echo "$GIT_OUTPUT";
 fi;

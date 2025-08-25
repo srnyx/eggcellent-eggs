06bc7ff4f38aec22589194c6782c1d4a53325786
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-25 17:56:29 -0400

    Add branch switching to Git eggs

--- Eggs/Git-Build-Java Application/installation.sh
+++ Eggs/Git-Build-Java Application/installation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 # Install dependencies
 apt update
@@ -9,21 +10,26 @@ mkdir -p /mnt/server
 cd /mnt/server || exit
 
 # Skip cloning/pulling if disabled
-if [ "${CLONE_INSTALL}" == "0" ]; then
+if [ "${CLONE_INSTALL:-1}" = "0" ]; then
   echo -e "Assuming user knows what they are doing, have a good day"
   exit 0
 fi
 
 # Add git ending if it's not on the address
-if [[ ${GIT_ADDRESS} != *.git ]]; then
-  GIT_ADDRESS=${GIT_ADDRESS}.git
+if [[ "${GIT_ADDRESS}" != *.git ]]; then
+  GIT_ADDRESS="${GIT_ADDRESS}.git"
 fi
 
 # Check for username and password (PAT)
-if [[ -z "${GIT_USERNAME}" && -z "${GIT_TOKEN}" ]]; then
+if [[ -z "${GIT_USERNAME:-}" && -z "${GIT_TOKEN:-}" ]]; then
   echo "Using anonymous API call (no username/password)"
 else
   GIT_ADDRESS="https://${GIT_USERNAME}:${GIT_TOKEN}@$(echo -e "${GIT_ADDRESS}" | cut -d/ -f3-)"
+fi
+
+# Trust /mnt/server for git operations if not already trusted
+if ! git config --global --get-all safe.directory | grep -qx "/mnt/server"; then
+  git config --global --add safe.directory /mnt/server
 fi
 
 if [ "$(ls -A /mnt/server)" ]; then
@@ -35,14 +41,20 @@ if [ "$(ls -A /mnt/server)" ]; then
     if [ -f .git/config ]; then
       echo "Loading info from git config"
       ORIGIN=$(git config --get remote.origin.url)
+      CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
     else
       echo "Files found with no git config; closing out without touching things to not break anything"
       exit 10
     fi
   fi
 
-  # Pull from git
-  if [ "${ORIGIN}" == "${GIT_ADDRESS}" ]; then
+  # Check branches
+  if [ -n "${GIT_BRANCH:-}" ] && [ "${CURRENT_BRANCH}" != "${GIT_BRANCH}" ]; then
+    echo "Switching from ${CURRENT_BRANCH} to ${GIT_BRANCH}"
+    git fetch origin "${GIT_BRANCH}:${GIT_BRANCH}"
+    git checkout "${GIT_BRANCH}"
+    git branch --set-upstream-to="origin/${GIT_BRANCH}" "${GIT_BRANCH}"
+  elif [ "${ORIGIN}" = "${GIT_ADDRESS}" ]; then
     echo "Pulling latest from git"
     git pull
   fi
@@ -50,7 +62,7 @@ else
   echo "/mnt/server IS empty; cloning files from repository"
 
   # Clone from git
-  if [ -z "${GIT_BRANCH}" ]; then
+  if [ -z "${GIT_BRANCH:-}" ]; then
     echo "Cloning default branch"
     git clone "${GIT_ADDRESS}" .
   else
